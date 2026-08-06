9fab8481dadae8acfad822c18dc261fd7f914e33
Fabio Ara <fabioeidiara@gmail.com>
2026-08-06 18:05:33 -0300

    Incrementa versão interna do APK

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -68,7 +68,7 @@ android {
         applicationId = "com.aralearn.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 153
+        versionCode = 154
         versionName = "0.0.14"
     }
 
